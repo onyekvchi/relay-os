3162edc263e630be49ec14f80dfd7fa2c8cc626c
Onyekachi Mbaike <onyekachi.mbaike@gmail.com>
2025-10-17 13:46:10 +0100

    Add app navbar and dashboard template

---
+++ components/app-navbar.vue
@@ -0,0 +1,58 @@
+<template>
+  <header class="bg-white border-b border-muted">
+      <div class="container mx-auto px-4 py-2 flex justify-between items-center">
+        <div class="flex gap-6 items-center">
+          <h1 class="text-sm font-bold font-mono tracking-tighter py-2 px-4 bg-primary text-inverted">relay-os</h1>
+          <UNavigationMenu orientation="horizontal" :items="navigationItems" :ui="{
+            list: 'gap-4',
+            link: 'px-2 py-2 text-sm gap-4',
+            linkLeadingIcon: 'size-4'
+          }"/>
+        </div>
+        <div class="flex items-center gap-4">
+          <UButton @click="handleLogout" variant="outline">
+            Logout
+          </UButton>
+        </div>
+      </div>
+    </header>
+</template>
+
+<script setup lang="ts">
+import { routes } from "@/routes";
+const { getUser, clearAuth } = useAuthStore();
+
+const handleLogout = async () => {
+  clearAuth();
+  await navigateTo(routes.signIn);
+};
+
+const { path } = useRoute()
+
+const navigationItems = [
+{
+  label: "Home",
+  to: "dashboard",
+  icon: "",
+  active: path.startsWith("dashboard"),
+},
+{
+  label: "Requests",
+  to: "requests",
+  icon: "",
+  active: path.startsWith("requests"),
+},
+{
+  label: "Workflows",
+  to: "workflows",
+  icon: "",
+  active: path.startsWith("workflows"),
+},
+{
+  label: "Metrics",
+  to: "metrics",
+  icon: "",
+  active: path.startsWith("metrics"),
+}
+]
+</script>
---
+++ layouts/dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen bg-background">
+    <app-navbar />
+    <main class="flex flex-col container mx-auto px-4 py-8 gap-8">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
