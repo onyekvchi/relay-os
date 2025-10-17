2272a1cc583bbb36cdaa9fa2f66bd9813eeaca88
Onyekachi Mbaike <onyekachi.mbaike@gmail.com>
2025-10-18 00:36:02 +0100

    Simplify auth layout and header with minimal design

--- components/auth/auth-header.vue
+++ components/auth/auth-header.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="flex flex-col space-y-6" :class="{ 'items-center text-center': center }">
-    <LogoImage />
     <div class="space-y-2">
-      <h1 class="text-3xl font-serif">{{ title }}</h1>
-      <p class="text-muted">{{ description }}</p>
+      <h1 class="text-2xl tracking-tight font-semibold">{{ title }}</h1>
+      <p class="text-muted text-sm">{{ description }}</p>
     </div>
   </div>
 </template>
--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,14 +1,12 @@
 <template>
-  <section class="min-h-screen flex justify-center relative px-4 pt-24 bg-muted">
+  <section class="min-h-screen flex justify-center relative px-4 pt-24 bg-white">
     <div class="w-full max-w-md z-10">
-      <UCard>
-        <div class="p-4 flex flex-col space-y-8">
-          <slot />
-        </div>
-      </UCard>
+      <div class="p-4 flex flex-col space-y-8">
+        <slot />
+      </div>
     </div>
     <div class="absolute bottom-0 left-0 right-0 h-full w-full">
-      <div class="w-xl h-full m-auto bg-[url(/assets/auth-bg.svg)] bg-contain bg-no-repeat bg-bottom"></div>
+      <div class="w-xl h-full m-auto"></div>
     </div>
   </section>
 </template>
